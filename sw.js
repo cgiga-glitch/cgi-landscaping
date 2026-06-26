336f44f562ed55696b972aa1295a590e3b0fb218
cgiga-glitch <cgiga@cgiholisticfitness.com>
2026-06-26 11:14:40 -0400

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,36 @@
+const CACHE = 'cgi-landscape-v1';
+const ASSETS = [
+  '/cgi-landscaping/',
+  '/cgi-landscaping/index.html',
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  // 구글 API는 캐시 안 함 (항상 최신 데이터)
+  if (e.request.url.includes('script.google.com')) return;
+
+  e.respondWith(
+    fetch(e.request)
+      .then(res => {
+        const clone = res.clone();
+        caches.open(CACHE).then(c => c.put(e.request, clone));
+        return res;
+      })
+      .catch(() => caches.match(e.request))
+  );
+});
